b0de31c07bea1e54374a106f9b7584fb41f2ecb9
Vlad.Zherko <vladikon19@tut.by>
2024-10-06 01:26:16 +0300

    controller endpoints

--- src/main/java/ru/clevertec/controller/ChannelController.java
+++ src/main/java/ru/clevertec/controller/ChannelController.java
@@ -1,7 +1,34 @@
 package ru.clevertec.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.clevertec.dto.ChannelDto;
+import ru.clevertec.dto.PaginatedChannelDto;
+import ru.clevertec.dto.request.ChannelFilter;
+import ru.clevertec.service.ChannelService;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
+@RequestMapping("/v1/channels")
+@RequiredArgsConstructor
 public class ChannelController {
+    private final ChannelService channelService;
+
+    @PostMapping
+    private ResponseEntity<ChannelDto> createChannel(@RequestBody ChannelDto channelDto){
+        ChannelDto channel = channelService.saveChannel(channelDto);
+
+        return ResponseEntity.status(CREATED)
+                .body(channel);
+    }
+
+    @GetMapping
+    private ResponseEntity<List<PaginatedChannelDto>> searchChannel(@RequestParam("page") Integer page,
+                                                                    @RequestParam("size") Integer size,
+                                                                    @RequestBody ChannelFilter channelFilter){
+        return ResponseEntity.ok(channelService.searchChannel(page, size, channelFilter));
+    }
 }
--- src/main/java/ru/clevertec/controller/UserController.java
+++ src/main/java/ru/clevertec/controller/UserController.java
@@ -1,7 +1,43 @@
 package ru.clevertec.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.clevertec.dto.UserDto;
+import ru.clevertec.service.UserService;
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/users")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    private ResponseEntity<UserDto> createUser(@RequestBody UserDto channelDto){
+        UserDto savedUser = userService.saveUser(channelDto);
+
+        return ResponseEntity.status(CREATED)
+                .body(savedUser);
+    }
+
+    @DeleteMapping("/{userId}")
+    private ResponseEntity<Void> deleteUser(@PathVariable("userId") UUID userId){
+        userService.deleteUser(userId);
+
+        return ResponseEntity.status(NO_CONTENT)
+                .build();
+    }
+
+    @PutMapping("/{userId}")
+    private ResponseEntity<Void> updateUser(@PathVariable("userId") UUID userId,
+                                            @RequestBody UserDto channelDto){
+       userService.fullUpdateUser(userId, channelDto);
+
+       return ResponseEntity.status(NO_CONTENT)
+               .build();
+    }
 }
