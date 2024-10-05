c84f354d7e36a0a2c896b42d406e098eedf807c3
Vlad.Zherko <vladikon19@tut.by>
2024-10-06 01:26:57 +0300

    added query

--- src/main/java/ru/clevertec/repository/ChannelRepository.java
+++ src/main/java/ru/clevertec/repository/ChannelRepository.java
@@ -1,10 +1,22 @@
 package ru.clevertec.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.entity.ChannelEntity;
 
-@Repository
-public interface ChannelRepository extends JpaRepository<ChannelEntity, Long> {
+import java.util.List;
+import java.util.UUID;
 
+@Repository
+public interface ChannelRepository extends JpaRepository<ChannelEntity, Long>,
+                                           JpaSpecificationExecutor<ChannelEntity>,
+                                           PagingAndSortingRepository<ChannelEntity, Long> {
+
+    @Query("SELECT c.title FROM ChannelEntity c JOIN c.subscribers s WHERE s.id = :subId")
+    List<String> findChannelTitlesBySubscriber(@Param("subId") UUID subscriberId);
 }
+
