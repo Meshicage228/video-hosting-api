491f30cd1ed478bb149fd94279607608e437b8e4
Vlad.Zherko <vladikon19@tut.by>
2024-10-17 16:50:11 +0300

    sessions service testing feign

--- session-service/src/main/java/ru/clevertec/sessionservice/controller/SessionController.java
+++ session-service/src/main/java/ru/clevertec/sessionservice/controller/SessionController.java
@@ -4,20 +4,24 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.clevertec.sessionservice.dto.SessionResponse;
+import ru.clevertec.sessionservice.entity.Session;
 import ru.clevertec.sessionservice.service.SessionService;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/v1/sessions")
 public class SessionController {
     private final SessionService sessionService;
 
     @GetMapping
-    public SessionResponse getOrCreateAndGetSession(@Valid @NotBlank(message = "should not be blank")
+    public Session getOrCreateAndGetSession(@Valid @NotBlank(message = "should not be blank")
                                                         @NotNull(message = "Login not provided")
                                                         @RequestParam("login") String login) {
-        return sessionService.findSessionByLogin(login);
+        SessionResponse sessionByLogin = sessionService.findSessionByLogin(login);
+        Session session = new Session(sessionByLogin.id(), sessionByLogin.login(), sessionByLogin.creationDate());
+        return session;
     }
 }
